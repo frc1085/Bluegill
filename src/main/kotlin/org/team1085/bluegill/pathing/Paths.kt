08bed9742892a4c2386571a8a15d965bb82e26bc
shmishtopher <shmish90@gmail.com>
2018-04-27 13:17:45 -0400

    added example path

--- src/main/kotlin/org/team1085/bluegill/pathing/Paths.kt
+++ src/main/kotlin/org/team1085/bluegill/pathing/Paths.kt
@@ -1,2 +1,12 @@
 package org.team1085.bluegill.pathing
 
+import jaci.pathfinder.Waypoint
+
+class Paths {
+  companion object {
+    val CROSS_LINE = arrayOf(
+      Waypoint(0.0, 0.0, 0.0),
+      Waypoint(3.0, 0.0, 0.0)
+    )
+  }
+}
