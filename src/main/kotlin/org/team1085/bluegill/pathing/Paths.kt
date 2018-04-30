9a15fae5ae5c4f8496e14add297f2e4cff0d60f7
shmishtopher <shmish90@gmail.com>
2018-04-30 17:49:53 -0400

    added path placeholders

--- src/main/kotlin/org/team1085/bluegill/pathing/Paths.kt
+++ src/main/kotlin/org/team1085/bluegill/pathing/Paths.kt
@@ -4,9 +4,21 @@ import jaci.pathfinder.Waypoint
 
 class Paths {
   companion object {
-    val CROSS_LINE = arrayOf(
+    public val CROSS_LINE = arrayOf(
       Waypoint(0.0, 0.0, 0.0),
       Waypoint(3.0, 0.0, 0.0)
+    )
+
+    public val LEFT_LEFT_SCALE = arrayOf(
+      Waypoint(0.0, 0.0, 0.0)
+    )
+
+    public val LEFT_RIGHT_SCALE = arrayOf(
+      Waypoint(0.0, 0.0, 0.0)
+    )
+
+    public val RIGHT_LEFT_SCALE = arrayOf(
+      Waypoint(0.0, 0.0, 0.0)
     )
   }
 }
